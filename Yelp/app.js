f20009c8b086dcba77d8b84652c3ba38f1b60c17
Daniel Olaya <d_olaya@hotmail.com>
2020-02-05 17:38:18 -0800

    Added mongo to app

--- Yelp/app.js
+++ Yelp/app.js
@@ -1,18 +1,42 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true,
+     useUnifiedTopology: true
+});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-   {name: "Tofino", image: "https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9"},
-   {name: "Parksville", image: "https://images.unsplash.com/photo-1542332213-1d277bf3d6c6?ixlib=rb-1.2.1"},
-   {name: "Cape Scott", image: "https://images.unsplash.com/photo-1488790881751-9068aa742b9b"},
-   {name: "Tofino1", image: "https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1"},
-   {name: "Parksville1", image: "https://images.unsplash.com/photo-1542332213-1d277bf3d6c6?ixlib=rb-1.2.1"},
-   {name: "Cape Scott1", image: "https://images.unsplash.com/photo-1488790881751-9068aa742b9b"}
-];
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+
+// Campground.create({
+//     name: "Parksville",
+//     image: "https://images.unsplash.com/photo-1542332213-1d277bf3d6c6?ixlib=rb-1.2.1",
+// }, function(err, campground){
+//     if (err){
+//         console.log("Error while inserting campground to db");
+//     } else {
+//         console.log("New campground inserted: ", campground);
+//     }
+// });
+
+
+// var campgrounds = [
+//    {name: "Tofino", image: "https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9"},
+//    {name: "Parksville", image: "https://images.unsplash.com/photo-1542332213-1d277bf3d6c6?ixlib=rb-1.2.1"},
+//    {name: "Cape Scott", image: "https://images.unsplash.com/photo-1488790881751-9068aa742b9b"},
+//    {name: "Tofino1", image: "https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1"},
+//    {name: "Parksville1", image: "https://images.unsplash.com/photo-1542332213-1d277bf3d6c6?ixlib=rb-1.2.1"},
+//    {name: "Cape Scott1", image: "https://images.unsplash.com/photo-1488790881751-9068aa742b9b"}
+// ];
 
 
 app.get("/", function(req, res){
@@ -20,15 +44,33 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds" ,{campgrounds: campgrounds});
+    Campground.find({},function (err, allCampgrounds){
+        if (err){
+            console.log("Oops, something went wrong searching for all campgrounds, ", err);
+        } else {
+            res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    });
+    // res.render("campgrounds" ,{campgrounds: campgrounds});
 });
 
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.render("campgrounds" ,{campgrounds: campgrounds});
+    // campgrounds.push(newCampground);
+    // res.render("campgrounds" ,{campgrounds: campgrounds});
+    Campground.create(newCampground, function(err, newlyCreated){
+        if (err){
+            console.log("Ooops, something went wrong creating new camp");
+        } else {
+            console.log("New campground inserted: ", newlyCreated);
+            res.render("campgrounds");
+        }
+    })
+
+
+
 });
 
 app.get("/campgrounds/new", function(req, res){
