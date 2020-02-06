4378c84a68fe9b877ab1d1573b4ab63a8080852b
Daniel Olaya <d_olaya@hotmail.com>
2020-02-06 11:51:55 -0800

    Blog stubs

---
+++ Blog/app.js
@@ -0,0 +1,49 @@
+var express = require("express"),
+    mongoose = require("mongoose"),
+    bodyParser = require("body-parser"),
+    app = express();
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost/blog", {useNewUrlParser: true,
+     useUnifiedTopology: true
+});
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+// MONGOOSE MODEL/CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose. model("Blog", blogSchema);
+
+
+// Blog.create({
+//     title: "Test1",
+//     image: "https://images.unsplash.com/photo-1518546305927-5a555bb7020d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80",
+//     body: "Hi this is Bitcorn"
+// });
+
+//RESTFUL ROUTES
+app.get("/", function(req, res){
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res){
+    res.render("index");
+    Blog.find({}, function(err, blogs){
+        if (err){
+            console.log(err);
+        } else {
+            console.log(blogs);
+            res.render("index", {blogs: blogs});
+        }
+    })
+});
+
+app.listen(3000, function(){
+    console.log("Blog server is running!");
+});
