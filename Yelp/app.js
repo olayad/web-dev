b06fb25b782b689f98971e23cb1baabb3a91eb84
Daniel Olaya <d_olaya@hotmail.com>
2020-02-25 16:13:56 -0800

    Hooks for adding new comments

--- Yelp/app.js
+++ Yelp/app.js
@@ -81,6 +81,26 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
     });
 });
 
+
+app.post("/campgrounds/:id/comments", function(req, res){
+   Campground.findById(req.params.id, function(err, campground){
+       if(err){
+           console.log(err);
+           res.redirect("/campgrounds");
+       } else{
+           Comment.create(req.body.comment, function(err, comment){
+               if (err){
+                   console.log(err);
+               } else {
+                   campground.comments.push(comment);
+                   campground.save();
+                   res.redirect('/campgrounds/'+campground._id)
+               }
+           })
+       }
+   })
+});
+
 app.listen(3000, function(){
     console.log("Yelp app started!");
 });
