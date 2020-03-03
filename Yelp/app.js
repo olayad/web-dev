616aad1cbdbd8a26884b911f8b911055e30e8288
Daniel Olaya <d_olaya@hotmail.com>
2020-03-03 11:44:17 -0800

    WIP: Edit campground

--- Yelp/app.js
+++ Yelp/app.js
@@ -7,7 +7,8 @@ var express     = require("express"),
     Campground  = require("./models/campground"),
     Comment     = require("./models/comment"),
     User        = require("./models/user"),
-    seedDB      = require("./seeds")
+    seedDB      = require("./seeds"),
+    methodOverride  =   require("method-override");
 
 //requring routes
 var commentRoutes    = require("./routes/comments"),
@@ -20,6 +21,7 @@ mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true,
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 // seedDB(); //seed the database
 
 // PASSPORT CONFIGURATION
