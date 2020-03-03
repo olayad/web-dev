3a8aeb302268eb4f2f12f6ec336c81b0e84955a4
Daniel Olaya <d_olaya@hotmail.com>
2020-03-02 20:49:24 -0800

    Author added to created campgrounds

--- Yelp/routes/campgrounds.js
+++ Yelp/routes/campgrounds.js
@@ -15,11 +15,15 @@ router.get("/", function(req, res){
 });
 
 // CREATE - Add new campground to db
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
-    var newCampground = {name: name, image: image, description: description};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var newCampground = {name: name, image: image, description: description, author: author};
     Campground.create(newCampground, function(err, newlyCreated){
         if (err){
             console.log("Ooops, something went wrong creating new camp");
@@ -31,7 +35,7 @@ router.post("/", function(req, res){
 });
 
 // NEW - Show form to create new campground
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
     res.render("campgrounds/new");
 });
 
@@ -45,5 +49,13 @@ router.get("/:id", function(req, res){
        }
     });
 });
+
+// middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
