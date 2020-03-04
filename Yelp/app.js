b6bac837fbd1c3ed566ea9f463d5730f732bd9f3
Daniel Olaya <d_olaya@hotmail.com>
2020-03-04 13:57:46 -0800

    Edit comments

--- Yelp/app.js
+++ Yelp/app.js
@@ -44,7 +44,7 @@ app.use(function(req, res, next){
 
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
-app.use("/campgrounds/:id/comments", commentRoutes)
+app.use("/campgrounds/:id/comments", commentRoutes);
 
 app.listen(3000, function(){
     console.log("Yelp app started!");
