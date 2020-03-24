44184a42c339b0f6c896caed3ec082c7e2e69352
Daniel Olaya <d_olaya@hotmail.com>
2020-03-23 17:10:02 -0700

    Recipe list component html added

--- Recipe-Book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Recipe-Book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'https://images.unsplash.com/photo-1495521821757-a1efb6729352?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80')
+  ];
+
+  
+
+
+  constructor() {
+  }
+
 
   ngOnInit(): void {
   }
