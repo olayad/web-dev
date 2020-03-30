d6ea6937ce9ecc4ab28f7a104e3fa66feb6b7da2
Daniel Olaya <d_olaya@hotmail.com>
2020-03-29 18:06:27 -0700

    WIP: Adding ingredient to shopping list

--- Recipe-Book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Recipe-Book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from "../../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, AfterViewInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(){
+  }
+
+  onAddItem(){
+    const newIngredient: Ingredient = {
+      name: this.nameInputRef.nativeElement.value,
+      amount: this.amountInputRef.nativeElement.value
+
+    };
+    this.ingredientAdded.emit(newIngredient);
+    this.clearInputs();
+  }
+
+  deleteIngredient(){
+    console.log("delete something");
+  }
+
+  clearInputs(){
+    this.nameInputRef.nativeElement.value = "";
+    this.amountInputRef.nativeElement.value = "";
   }
 
 }
--- Recipe-Book/src/app/shopping-list/shopping-list.component.ts
+++ Recipe-Book/src/app/shopping-list/shopping-list.component.ts
@@ -17,4 +17,6 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+
 }
