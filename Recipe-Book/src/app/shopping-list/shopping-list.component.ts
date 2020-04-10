7c2a6393d55374b4469a53449a7a0ccfc8c42fcc
Daniel Olaya <d_olaya@hotmail.com>
2020-04-10 10:36:26 -0700

    Replaced eventemitter for subjects

--- Recipe-Book/src/app/shopping-list/shopping-list.component.ts
+++ Recipe-Book/src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model'
 import {ShoppingListService} from "./shopping-list.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-shopping-list',
@@ -8,14 +9,15 @@ import {ShoppingListService} from "./shopping-list.service";
   styleUrls: ['./shopping-list.component.css'],
 
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy{
   ingredients: Ingredient[] = [];
+  private igChangeSub: Subscription;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
     this.ingredients = this.shoppingListService.getIngredients()
-    this.shoppingListService.ingredientsChanged
+    this.igChangeSub = this.shoppingListService.ingredientsChanged
       .subscribe(
         (ingredients: Ingredient[]) => {
           this.ingredients = ingredients;
@@ -23,8 +25,11 @@ export class ShoppingListComponent implements OnInit {
   }
 
   addIngredient(ingredient: Ingredient){
-    console.log('shoppinglist add ingredient ', ingredient)
-    this.shoppingListService.addIngredient(ingredient)
+    this.shoppingListService.addIngredient(ingredient);
+  }
+
+  ngOnDestroy(): void {
+    this.igChangeSub.unsubscribe();
   }
 
 }
--- Recipe-Book/src/app/shopping-list/shopping-list.service.ts
+++ Recipe-Book/src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,10 @@
 import {Ingredient} from "../shared/ingredient.model";
-import {EventEmitter, Injectable} from "@angular/core";
+import {Injectable} from "@angular/core";
+import {Subject} from "rxjs";
 
 @Injectable({providedIn:'root'})
 export class ShoppingListService {
-  ingredientsChanged  = new EventEmitter<Ingredient[]>();
+  ingredientsChanged  = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Pear', 10),
@@ -12,12 +13,12 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient){
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.getIngredients())
+    this.ingredientsChanged.next(this.getIngredients())
   }
 
   addIngredients(ingredients: Ingredient[]){
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.getIngredients())
+    this.ingredientsChanged.next(this.getIngredients())
   }
 
   getIngredients(){
